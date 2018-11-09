b683e269d77cef2b09f47a17fe059f95bbfbb9b4
CHN777 <j645236539@icloud.com>
2018-11-09 14:43:32 +0800

    组件化改造

--- 01/vue-demo/src/components/todolist.vue
+++ 01/vue-demo/src/components/todolist.vue
@@ -1,23 +1,63 @@
 <template>
     <div>
-    <input @keyup.enter="handleClick" type="text" v-model="newLesson" />
-      <button @click="handleClick()">add</button>
+        <HeaderAera/>
+        <input @keyup.enter="handleClick" type="text" v-model="newLesson" />
+        <button @click="handleClick()">add</button>
 
-      <ul>
-        <li :class="{ss:list.done}" @click="change(id)" v-for="(list,id) in lists">
-          {{ list.name }}
-        </li>
-      </ul>
-      <div>{{cremain}}</div>
+        <ul>
+            <li v-bind:class="{ss:list.done}" v-on:click="change(id)" v-for="(list,id) in lists">
+            {{ list.name }}
+            </li>
+        </ul>
+        <div>{{cremain}}</div>
     </div>
 </template>
 
 <script>
-    export default {
-        
+
+import HeaderAera from './HeaderAera'
+
+export default 
+{
+    components:{
+        HeaderAera
+    },
+    data() 
+        {
+            return {
+                "lists": [
+                {name:'python',done:false},
+                {name:'nodejs',done:false},
+                {name:'vue',done:false}
+                ],
+                newLesson:"",
+            }
+        },
+        methods: {
+        handleClick() {
+            this.lists.push({name:this.newLesson,done:false});
+        },
+        change(id) {
+            this.lists[id].done = !this.lists[id].done;
+        },
+
+    },
+    computed: {
+        lineThrow() {
+            return {
+            "text-decoration": "line-through"
+            }
+        },
+        cremain() {
+        return this.lists.filter(v=>{return v.done===false}).length;
+        }
     }
+}
 </script>
 
 <style scoped>
-
+    .ss{
+        text-decoration: line-through;
+        color : red
+      }
 </style>
--- 01/vue-demo/src/main.js
+++ 01/vue-demo/src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import TodoList from './components/TodoList'
 
 Vue.config.productionTip = false
 
@@ -15,33 +16,9 @@ Vue.config.productionTip = false
 // })
 new Vue({
   el:"#app",
-  data() {
-    return {
-      "lists": [
-        {name:'python',done:false},
-        {name:'nodejs',done:false},
-        {name:'vue',done:false}
-      ],
-      newLesson:"",
-    }
+  components:{
+    TodoList
   },
-  methods: {
-    handleClick() {
-      this.lists.push({name:this.newLesson,done:false});
-    },
-    change(id) {
-      this.lists[id].done = !this.lists[id].done;
-    },
-    
-  },
-  computed: {
-    lineThrow() {
-      return {
-        "text-decoration": "line-through"
-      }
-    },
-    cremain() {
-      return this.lists.filter(v=>{return v.done===false}).length;
-     }
-  }
+  template:"<TodoList />"
+  
 })

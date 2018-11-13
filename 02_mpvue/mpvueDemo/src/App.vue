944c126fe563c29d38c5549005ea08f9bef0bf06
CHN777 <j645236539@icloud.com>
2018-11-13 11:03:19 +0800

    新增测试扫描接口

--- 02_mpvue/mpvueDemo/src/App.vue
+++ 02_mpvue/mpvueDemo/src/App.vue
@@ -1,31 +1,98 @@
 <script>
 
-import my_util from './my_util';
+import my_util from '@/my_util';
+import config from '@/my_config'
+import qcloud from 'wafer2-client-sdk'
 
 export default {
-  async created () {
+  data () {
+    return {
+      userinfo: {
+        avatarUrl: 'http://image.shengxinjing.cn/rate/unlogin.png',
+        nickName: ''
+      }
+    }
+  },
+  methods:
+  {
+      //简单登录测试
+      myloging:function () {
+        qcloud.setLoginUrl(config.loginUrl);
+        qcloud.login({
+          success: function (userInfo) {
+            console.log('登录成功', userInfo);
+          },
+          fail: function (err) {
+            console.log('登录失败', err);
+          }
+        });
 
-    const res = await my_util.get('/weapp/demo');
-    console.log(123,res);
-  }
+      },
+      loginSuccess (res)
+      {
+        wx.showToast({
+          title: '登录成功'
+        });
+        wx.setStorageSync('userinfo', res);
+        this.userinfo = res
+      },
+      login ()
+      {
+        wx.showToast({
+          title: '登录中',
+          icon: 'loading'
+        });
+        qcloud.setLoginUrl(config.loginUrl);
+        const session = qcloud.Session.get();
+        if (session)
+        {
+          qcloud.loginWithCode({
+            success: res => {
+              console.log('没过期的登录', res);
+              this.loginSuccess(res)
+            },
+            fail: err => {
+              console.error(err)
+            }
+          });
+        }
+        else
+        {
+          qcloud.login({
+            success: res => {
+              console.log('登录成功', res);
+              this.loginSuccess(res)
+            },
+            fail: err => {
+              console.error(err)
+            }
+          });
+        }
+      }
+    },
+    async created ()
+    {
+      const res = await my_util.get('/weapp/demo');
+      console.log(123,res);
+      await this.login()
+    }
 }
 </script>
 
 <style>
-.container {
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-  padding: 200rpx 0;
-  box-sizing: border-box;
-}
-/* this rule will be remove */
-* {
-  transition: width 2s;
-  -moz-transition: width 2s;
-  -webkit-transition: width 2s;
-  -o-transition: width 2s;
-}
+  .btn{
+    color:white;
+    background:#EA5A49;
+    margin-bottom: 10px;
+    padding-left: 15px;
+    padding-left: 15px;
+    border-radius: 2px;
+    font-size: 16px;
+    line-height: 40px;
+    height: 40px;
+    width: 100%;
+  }
+  .btn:active{
+    background: #FA5A49;
+  }
 </style>
--- 02_mpvue/mpvueDemo/src/pages/index/index.vue
+++ 02_mpvue/mpvueDemo/src/pages/index/index.vue
@@ -1,25 +1,29 @@
 <template>
   <div>
-      主页
+
 
     <div class="container">
       <div class="userinfo" >
         <img :src="userinfo.avatarUrl" alt="">
         <p>{{userinfo.nickName}}</p>
+
       </div>
+
+      <year-page-progress></year-page-progress>
+
       <button v-if='userinfo.openId' @click='scanBook' class='btn'>添加图书</button>
       <button v-else open-type="getUserInfo" lang="zh_CN" class='btn' @getuserinfo="login">点击登录</button>
 
     </div>
 
+
   </div>
 </template>
 
 <script>
-
-  import qcloud from 'wafer2-client-sdk'
-  import config from '@/my_config'
+  import YearPageProgress from '@/components/YearProgress'
   export default {
+    components: {YearPageProgress},
     data () {
       return {
         userinfo: {
@@ -28,64 +32,45 @@
         }
       }
     },
-    methods:
-    {
-      loginSuccess (res)
-      {
-        wx.showToast({
-          title: '登录成功'
-        });
-        wx.setStorageSync('userinfo', res);
-        this.userinfo = res
-      },
-      login ()
-      {
-        wx.showToast({
-          title: '登录中',
-          icon: 'loading'
-        });
-        qcloud.setLoginUrl(config.loginUrl);
-        const session = qcloud.Session.get();
-        if (session)
-        {
-          qcloud.loginWithCode({
-            success: res => {
-              console.log('没过期的登录', res);
-              this.loginSuccess(res)
-            },
-            fail: err => {
-              console.error(err)
-            }
-          });
-        }
-        else
-        {
-          qcloud.login({
-            success: res => {
-              console.log('登录成功', res);
-              this.loginSuccess(res)
-            },
-            fail: err => {
-              console.error(err)
-            }
-          });
-        }
+    methods:{
+      scanBook() {
+        wx.scanCode({
+          success (res) {
+            console.log(res)
+          }
+        })
       }
-    },
-    onShow ()
-    {
-      this.login();
+    }
+    ,
+    created(){
       wx.showShareMenu();
       let userinfo = wx.getStorageSync('userinfo');
-      if (userinfo) {
+      if (userinfo)
+      {
         this.userinfo = userinfo
       }
     }
   }
 </script>
 
-<style scoped >
+<style scoped lang='scss'>
+  .container
+  {
+    padding:0 30rpx;
 
+  }
+  .userinfo
+  {
+    margin-top:100rpx;
+    text-align:center;
+    img
+    {
+      width: 150rpx;
+      height:150rpx;
+      margin: 20rpx;
+      border-radius: 50%;
+    }
+  }
 
 
 </style>
